5bc7a58c40cb97f35bda3a199846f8e50ff40be4
thi131190 <thi131190@gmail.com>
2019-12-02 11:02:09 +0700

    Add React Router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
-
+import { Switch, Route } from "react-router-dom";
+import SignUp from "./pages/Signup";
+import Login from "./pages/Login";
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
+      <Switch>
+        <Route path="/signup">
+          <SignUp />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+      </Switch>
     </div>
   );
 }
